ac0c178b9f811aaefbd6c45fef56a104f935bbbf
Meuter <eelco.meuter@alliander.com>
2018-02-02 14:21:29 +0100

    implemented clcik event

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  result: string;
+
+  search: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSearch() {
+    this.result = this.search;
+  }
 }
